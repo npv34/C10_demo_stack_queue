ea3a57e90b82c6913f4dc20fe291091088b668e8
npv34 <phanvanluan611996@gmail.com>
2022-12-16 15:28:06 +0700

    demo stack and queue

---
+++ StackInterface.ts
@@ -0,0 +1,13 @@
+interface StackInterface<T> {
+    container: T[];
+    size: number;
+
+    push(item: T): void;
+    pop(): T;
+    getSize(): number;
+    isFull(): boolean;
+    isEmpty(): boolean;
+
+}
+
+export default StackInterface;
